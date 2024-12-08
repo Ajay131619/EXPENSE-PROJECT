744880662346a926726c4a7cde92bdfcde195628
ajay131619 <pachhinela.ajay@gmail.com>
2024-12-08 19:17:15 +0530

    script for mysql server

--- mysql.sh
+++ mysql.sh
@@ -60,7 +60,6 @@ installation(){
         then 
             echo -e " mysql is $g already installed ! $n "
             echo " nothing to do!!"
-            check_status
             exit 1
         else
             echo -e " mysql is $r not installed in your system ! $n "
